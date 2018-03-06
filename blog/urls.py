5e028750e48b12e93fcd3c7b312b23d970856fef
Sindre-s <sindre.schj@gmail.com>
2018-03-06 13:08:06 +0100

    added auth edit, and added registration form

--- blog/urls.py
+++ blog/urls.py
@@ -5,10 +5,11 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.post_list, name='view_blogs'),
-	url(r'post$',views.post_list),
-	url(r'create_blog$',views.create_blog, name='create_blog'),
+	#url(r'post$',views.post_list),
+	#url(r'create_blog$',views.create_blog, name='create_blog'),
 	url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 	url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
 	url(r'^post/new/$', views.post_new, name='post_new'),
 	url(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
+	url(r'^signup/$', views.signup, name='signup')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,32 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm, CommentForm
 from django.contrib.auth.decorators import login_required
+from .decorators import user_is_post_author
 
 
 # Create your views here.
 
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('.')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-	
+
 	context = {
 			'posts':posts
 	}
@@ -18,7 +34,7 @@ def post_list(request):
 	return render(request, 'blog/view_blogs.html', context)
 
 #@login_required
-def create_blog(request):
+#def create_blog(request):
 	if(request.method =='POST'):
 		print "request.POST", request.POST
 		title = request.POST['title']
@@ -36,7 +52,8 @@ def create_blog(request):
 				'users':users
 		}
 		return render(request, 'blog/create_blog.html', user)
-@login_required		
+
+@login_required
 def post_new(request):
 	if request.method == "POST":
 		form = PostForm(request.POST)
@@ -49,12 +66,14 @@ def post_new(request):
 	else:
     		form = PostForm()
     	return render(request, 'blog/post_edit.html', {'form': form})
-    
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
- 
-@login_required   
+
+
+@user_is_post_author
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -81,4 +100,3 @@ def add_comment_to_post(request, pk):
 	else:
 		form = CommentForm()
 	return render(request, 'blog/add_comment_to_post.html', {'form' : form})
-
